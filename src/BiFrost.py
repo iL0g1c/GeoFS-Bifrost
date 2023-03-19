30dc7272807fb61d86a08b53a9054f4a1292724d
iL0g1c <mathtodd05@gmail.com>
2023-03-18 20:31:30 -0400

    small changes

--- src/BiFrost.py
+++ src/BiFrost.py
@@ -13,7 +13,7 @@ intents = discord.Intents.all()
 load_dotenv()
 BOT_TOKEN = os.getenv("DISCORD_TOKEN")
 geofs_session_id = os.getenv("GEOFS_SESSION_ID")
-bot = commands.Bot(intents=intents, command_prefix="bifrost! ")
+bot = commands.Bot(intents=intents, command_prefix="bf! ")
 CATALOG_DIR = "catalog/"
 
 def setup():
