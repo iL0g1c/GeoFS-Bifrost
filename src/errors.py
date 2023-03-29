b62cb451e068d2f725ed802541bdfca54a100b47
iL0g1c <mathtodd05@gmail.com>
2023-03-29 17:32:10 -0400

    rewrote for python 3.9 support.

--- src/errors.py
+++ src/errors.py
@@ -1,13 +1,12 @@
 
 def getErrors(errorCode):
-    match errorCode:
-        case 1:
-            return "Your server has no blocked users."
-        case 2:
-            return "Internal Server Error: 'guilds.jsonl' does not exist.\n Please contact OspreyEyes."
-        case 3:
-            return "Incorrect value for the Account ID parameter."
-        case 4:
-            return "Internal Server Error: 'data.json' does not exist.\n Please contact OspreyEyes."
-        case 5:
-            return "Incorrect value for the Account ID parameter."
+    if errorCode == 1:
+        return "Your server has no blocked users."
+    elif errorCode == 2:
+        return "Internal Server Error: 'guilds.jsonl' does not exist.\n Please contact OspreyEyes."
+    elif errorCode == 3:
+        return "Incorrect value for the Account ID parameter."
+    elif errorCode == 4:
+        return "Internal Server Error: 'data.json' does not exist.\n Please contact OspreyEyes."
+    elif errorCode == 5:
+        return "Incorrect value for the Account ID parameter."
